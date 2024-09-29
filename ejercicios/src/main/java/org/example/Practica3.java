ec5a3cb024fb702261c6ba86195744ab8b2ad17a
dgarsau <1garsau@gmail.com>
2024-09-30 01:21:04 +0200

    Práctica 3

--- ejercicios/src/main/java/org/example/Practica3.java
+++ ejercicios/src/main/java/org/example/Practica3.java
@@ -8,6 +8,46 @@ public class Practica3 {
 
         Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Introduce tu nombre:");
+        String nombre = entrada.next();
+
+        int edad = -1;
+
+        System.out.println("Hola " + nombre + ".");
+
+        while(edad<0){
+            try{
+                System.out.println("Introduce tu edad:");
+                edad = entrada.nextInt();
+
+                if(edad<0){
+                    System.out.println("Error. La edad no puede ser un número negativo.");
+                }
+            }
+            catch (NumberFormatException | InputMismatchException e) {
+                System.out.println("Error. La edad tiene que ser un número válido.");
+                entrada.nextLine();
+            }
+        }
+
+        if (edad >= 18) {
+            System.out.println("Enhorabuena " + nombre + ". Puedes votar.");
+
+        } else {
+            System.out.println("Lo siento " + nombre + ". No puedes votar.");
+            System.out.println("Te faltan " + (18 - edad) + " años para ser mayor de edad.");
+        }
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
