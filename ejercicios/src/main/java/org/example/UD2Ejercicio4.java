83d5f6bd1d6d6c2911f8dc35f1df58805ec15651
A714\1dam <1garsau@gmail.com>
2024-10-08 20:05:51 +0200

    Sigo con el ejercicio 4

--- ejercicios/src/main/java/org/example/UD2Ejercicio4.java
+++ ejercicios/src/main/java/org/example/UD2Ejercicio4.java
@@ -3,6 +3,31 @@ package org.example;
 public class UD2Ejercicio4 {
     public void UD2ejercicio4(){
 
+        String texto = "Hola Mundo, bienvenido a Mundo. Mundo es genial.";
+        String subcadena = "Mundo";
+
+
+
+
+//        while () {
+//            int posicion = texto.indexOf(subcadena);
+//            System.out.println(posicion);
+//
+//        }
+
+        int posicion = texto.indexOf(subcadena);
+        System.out.println(posicion);
+
+        int posicion2 = texto.indexOf(subcadena,posicion+1);
+        System.out.println(posicion2);
+
+        int posicion3 = texto.indexOf(subcadena,posicion2+1);
+        System.out.println(posicion3);
+
+        int posicion4 = texto.indexOf(subcadena,posicion3+1);
+        System.out.println(posicion4);
+
+
     }
 
 }
