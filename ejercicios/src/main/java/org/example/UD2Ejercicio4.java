5a612bf2ec37b7d1a8ec3d7f1f7ce527c3513440
Daniel García Saugar <1garsau@gmail.com>
2024-10-10 00:01:55 +0200

    termino el ejercicio 4 y empiezo el 5

--- ejercicios/src/main/java/org/example/UD2Ejercicio4.java
+++ ejercicios/src/main/java/org/example/UD2Ejercicio4.java
@@ -1,31 +1,66 @@
 package org.example;
+import java.util.Scanner;
 
 public class UD2Ejercicio4 {
     public void UD2ejercicio4(){
 
-        String texto = "Hola Mundo, bienvenido a Mundo. Mundo es genial.";
-        String subcadena = "Mundo";
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("PROGRAMA PARA VER CUANTAS VECES APARECE UNA PALABRA EN UN TEXTO");
+        System.out.println("Introduce el texto:");
+        String palabra = entrada.nextLine();
+
+        System.out.println("Introduce la palabra:");
+        String subcadena = entrada.nextLine();
+
+
+        int posicion = 0;
+        int veces = 0;
+
+        posicion = palabra.indexOf(subcadena,posicion);
+
+        while (posicion>=0) {
+
+            posicion = palabra.indexOf(subcadena,posicion+1);
+            veces +=1;
+
+        }
+
+        System.out.println("La palabra aparece " + veces + " veces en el texto.");
 
 
 
+//        ASÍ LO HICE AL PRINCIPIO PARA VER COMO PODRÍA HACERLO DEJANDO QUE EL USUARIO
+//        INTRODUZCA LAS VARIABLES, EL WHILE SE ME OCURRIO PORQUE AL AÑADIR UNO A LA
+//        A LA POSICIÓN DE LA ÚLTIMA PALABRA ME DABA SIEMPRE VALOR -1, LA LINEA DE ANTES
+//        DEL WHILE LA HE PUESTO PORQUE COMO SUMO UNO A LA POSICION DENTRO DEL WHILE, NO ME
+//        CONTABA LAS PALABRAS QUE ESTUVIESEN AL PRINCIPIO DEL TEXTO
 
-//        while () {
-//            int posicion = texto.indexOf(subcadena);
-//            System.out.println(posicion);
+
+//        String texto = "Hola Mundo, bienvenido a Mundo. Mundo es genial.";
+//        String subcadena = "Mundo";
 //
-//        }
-
-        int posicion = texto.indexOf(subcadena);
-        System.out.println(posicion);
-
-        int posicion2 = texto.indexOf(subcadena,posicion+1);
-        System.out.println(posicion2);
-
-        int posicion3 = texto.indexOf(subcadena,posicion2+1);
-        System.out.println(posicion3);
-
-        int posicion4 = texto.indexOf(subcadena,posicion3+1);
-        System.out.println(posicion4);
+//        int veces = 0;
+//
+//        int posicion = texto.indexOf(subcadena);
+//        System.out.println(posicion);
+//
+//        veces += 1;
+//
+//        int posicion2 = texto.indexOf(subcadena,posicion+1);
+//        System.out.println(posicion2);
+//
+//        veces += 1;
+//
+//        int posicion3 = texto.indexOf(subcadena,posicion2+1);
+//        System.out.println(posicion3);
+//
+//        veces += 1;
+//
+//        int posicion4 = texto.indexOf(subcadena,posicion3+1);
+//        System.out.println(posicion4);
+//
+//        System.out.println("El texto aparece " + veces + " veces.");
 
 
     }
---
+++ ejercicios/src/main/java/org/example/UD2Ejercicio5.java
@@ -0,0 +1,45 @@
+package org.example;
+import java.util.Scanner;
+
+public class UD2Ejercicio5 {
+
+    public void UD2ejercicio5() {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("JUEGO DE ADIVINAR UNA PALABRA QUE ESTE EN UN TEXTO");
+
+        System.out.println("JUGADOR 1: Introduce un texto:");
+        String texto = entrada.nextLine();
+
+        System.out.println("JUGADOR 2: Introduce una palabra:");
+        String palabra = entrada.nextLine();
+
+        int contador = 10;
+
+        while (contador > 0 && !texto.contains(palabra)) {
+
+            if (!texto.contains(palabra)) {
+                System.out.println("La palabra no aparece en el texto.");
+                System.out.println("Te quedan " + contador + " intentos");
+                System.out.println("Introduce otra palabra:");
+                palabra = entrada.nextLine();
+                contador -= 1;
+            }
+
+            else {
+                System.out.println("Felicidades! La palabra aparece en el texto.");
+            }
+
+        }
+
+        System.out.println("No te quedan intentos, gana el Jugador 1");
+
+    }
+
+}
+
+
+
+
+
