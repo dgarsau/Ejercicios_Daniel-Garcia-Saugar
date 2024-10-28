bd639de436d3193320cdd6a233730fd5fc944783
A714\1dam <1garsau@gmail.com>
2024-10-28 20:13:08 +0100

    Termino numeros primos

--- ejercicios/src/main/java/org/example/Ampliacion1.java
+++ ejercicios/src/main/java/org/example/Ampliacion1.java
@@ -8,7 +8,7 @@ public class Ampliacion1 {
         Scanner entrada = new Scanner(System.in);
 
         int primo = 0;
-        boolean primo1 = false;
+        boolean primo1 = true;
         String secuencia = "Números primos: ";
 
         System.out.println("¿Hasta que número quieres?");
@@ -17,14 +17,16 @@ public class Ampliacion1 {
         for (int i = 2; i < cantidad; i++) {
             primo1=true;
 
-            for (int j = 2; j < cantidad; j++) {
+            for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     primo1=false;
                     break;
                 }
             }
 
-
+            if (primo1){
+               secuencia=secuencia.concat(" ," + i);
+            }
         }
 
         System.out.println(secuencia);
