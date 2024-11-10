52ff6eabc8fd14b680859ece3db6aeaab8b4348b
Daniel García Saugar <1garsau@gmail.com>
2024-11-10 18:45:37 +0100

    Termino samuráis.

--- ejercicios/src/main/java/org/example/UD3Practica1.java
+++ ejercicios/src/main/java/org/example/UD3Practica1.java
@@ -1,60 +1,80 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class UD3Practica1 {
-    public void UD3practica1(){
+    public void UD3practica1() {
 
         Scanner entrada = new Scanner(System.in);
         Random random = new Random();
 
-        String equipo[] = new String[7];
+        String potencia;
 
-        int equipo_int[]= new int [7];
+        String[] equipo = new String[7];
+        int[] equipo_int = new int[7];
 
-        int equipo1[]= new int [7];
-        int equipo2[]= new int [7];
+        int[] equipo1 = new int[7], equipo2 = new int[7];
 
-        String potencia = "";
-
-        int total=0;
-        int aux;
-        boolean error=true;
-
-        int primer_samurai= random.nextInt(1,7);
-
-
-
+        int total, aux;
         int vuelta=1;
 
-        //potencia=potencia.replaceAll("\\D+", " ");
+        boolean error=true, longitud=true, formato;
+
+        int samurai = random.nextInt(7) + 1;
+        int puntos1=0, puntos2=0;
+
 
         do {
+            while (error) {
 
-            while(error) {
+                total = 0;
+                formato = false;
 
-                System.out.println("Introduce la potencia de los samurais (Equipo "+vuelta+"):");
-                potencia = entrada.nextLine();
-                potencia=potencia.replaceAll("\\D+", " ");
-                equipo = potencia.split(" ");
+                while (longitud) {
+                    System.out.println("EQUIPO " + vuelta);
+                    System.out.println("Introduce la potencia de los samurais");
+                    System.out.println("(7 numeros, separados por espacios, 30 puntos en total.)");
+                    potencia = entrada.nextLine();
+                    potencia = potencia.trim();
+                    equipo = potencia.split(" ");
+
+                    if (equipo.length != 7) {
+                        System.out.println("ERROR. Se tienen que introducir 7 samuráis.");
+                        System.out.println("________________________________________________________");
+                    } else {
+                        longitud = false;
+                    }
+                }
+
+                longitud=true;
 
                 for (int i = 0; i < equipo.length; i++) {
-                    aux = Integer.parseInt(equipo[i]);
-                    equipo_int[i] = aux;
-                    total += aux;
+                    try {
+                        aux = Integer.parseInt(equipo[i]);
+                        equipo_int[i] = aux;
+                        total += aux;
+                    } catch (NumberFormatException e) {
+                        formato = true;
+                    }
                 }
-                if (total != 30) {
-                    System.out.println("ERROR. La suma de la potencia no es 30.");
-                    total = 0;
+
+                if (formato) {
+                    System.out.println("ERROR. Entrada no válida (Tienen que ser números)");
+                    System.out.println("________________________________________________________");
+                } else if (total != 30) {
+                    System.out.println("ERROR. La potencia total no es 30.");
+                    System.out.println("________________________________________________________");
                 } else {
-                    error = false;
+                    System.out.println("Equipo completado!");
+                    System.out.println("________________________________________________________");
+                    error=false;
                 }
             }
 
             error=true;
-            total = 0;
 
             switch (vuelta) {
                 case 1:
@@ -64,83 +84,49 @@ public class UD3Practica1 {
                     equipo2 = equipo_int.clone();
                     break;
             }
-
             vuelta++;
-
         }
-
-        while(vuelta<3);
+        while (vuelta < 3);
 
         System.out.println("¡Empieza la batalla!");
-        System.out.println("La batalla empieza con el Samurai " + primer_samurai);
+        System.out.println("La batalla empieza con el Samurai " + samurai);
+        System.out.println();
 
-        for (int i=0; i<equipo.length ; i++){
-            if(equipo1[primer_samurai+i]>equipo2[primer_samurai+i]){
-                System.out.println("Samurai "+ (primer_samurai+i) + ". Gana Equipo 1. " +equipo1[primer_samurai+i]+ " vs " + equipo2[primer_samurai+i]);
+        while (puntos1 < 4 && puntos2 < 4) {
+
+            if (samurai > 7) {
+                samurai = 1;
             }
 
+            if (equipo1[samurai - 1] > equipo2[samurai - 1]) {
+                System.out.println("Samurai " + samurai + ": Gana Equipo 1. " + equipo1[samurai - 1] + " vs " + equipo2[samurai - 1]);
+                equipo2[samurai - 1] = 0;
+                puntos1++;
+            } else if (equipo1[samurai - 1] < equipo2[samurai - 1]) {
+                System.out.println("Samurai " + samurai + ": Gana Equipo 2. " + equipo1[samurai - 1] + " vs " + equipo2[samurai - 1]);
+                equipo1[samurai - 1] = 0;
+                puntos2++;
+            } else {
+                System.out.println("Samurai " + samurai + ": Empate, los dos samuráis mueren.");
+                equipo1[samurai - 1] = 0;
+                equipo2[samurai - 1] = 0;
+                puntos1++;
+                puntos2++;
+            }
+            samurai++;
         }
 
+        System.out.println();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//            if (total != 30) {
-//                total = 0;
-//            } else {
-//                error = false;
-//            }
-//        }
-
-//        while(error) {
-//
-//            System.out.println("Introduce la potencia de los samurais (Equipo 1):");
-//
-//            for (int i = 0; i < equipo1.length; i++) {
-//                equipo1[i] = entrada.nextInt();
-//                total1 += equipo1[i];
-//            }
-//            if (total1!=30){
-//                total1=0;
-//                System.out.println("ERROR. La potencia total no suma 30.");
-//            }else{
-//                System.out.println("Equipo completado.");
-//                error=false;
-//            }
-//        }
-//
-//        error=true;
-//
-//        while (error) {
-//
-//            System.out.println("Introduce la potencia de los samurais (Equipo 2):");
-//
-//            for (int i = 0; i < equipo2.length; i++) {
-//                equipo2[i] = entrada.nextInt();
-//                total2 += equipo1[i];
-//            }
-//            if (total2 != 30) {
-//                total1 = 0;
-//                System.out.println("ERROR. La potencia total no suma 30.");
-//            } else {
-//                System.out.println("Equipo completado.");
-//                error = false;
-//            }
-//        }
-
-
-
+        if(puntos1>puntos2){
+            System.out.println("Equipo 1 GANA! Equipo 2 ha tenido " + puntos1 + " bajas.");
+        }else if (puntos1<puntos2){
+            System.out.println("Equipo 2 GANA! Equipo 1 ha tenido " + puntos2 + " bajas.");
+        }else {
+            System.out.println("Empate. Los dos equipos han tenido 4 bajas.");
+        }
     }
 }
+
+
+
